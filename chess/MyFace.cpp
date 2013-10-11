6b818924ef44ceee465d8295bc0b2cb80c8869ac
finlab <zgl92@qq.com>
2013-10-12 00:55:34 +0800

    first commit

---
+++ chess/MyFace.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "MyFace.h"
+
+
+MyFace::MyFace(void)
+{
+}
+
+
+MyFace::~MyFace(void)
+{
+}
---
+++ chess/MyFace.h
@@ -0,0 +1,19 @@
+#pragma once
+
+class MyFace
+{
+public:
+	unsigned char red[16];
+	unsigned char black[16];
+	int next;
+
+public:
+	MyFace(void){}
+	~MyFace(void){}
+
+	int evaluate();
+	bool issame(MyFace& f)
+};
+
+
+
